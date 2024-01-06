5447729b656b142a48857a36762d58bf43e5e2a1
Louis Poirier <contact@louispoirier.io>
2024-01-06 17:16:28 +0100

    fix: add gamepad support for Chrome.

--- src/listener.ts
+++ src/listener.ts
@@ -6,7 +6,14 @@ import {ButtonReleasedEvent, JoystickMovedEvent} from "./gamepad-event";
  * Whether browser support Gamepad API?
  */
 function isCompatible(): boolean {
-  return !!navigator.getGamepads && 'ongamepadconnected' in window;
+  return !!navigator.getGamepads;
+}
+
+/**
+ * Whether browser emit events to detect a gamepad?
+ */
+function canListenGamepadEvents(): boolean {
+  return 'ongamepadconnected' in window;
 }
 
 /**
@@ -18,9 +25,14 @@ export function start(): void {
       'version. Chrome / Firefox / Edge are valid candidates.');
     return;
   }
-  window.addEventListener('gamepadconnected', onConnected);
-  window.addEventListener('gamepaddisconnected', onDisconnected);
   document.addEventListener('keyup', onKeyUp);
+  if (!canListenGamepadEvents()) {
+    pollGamepad();
+    Plugin.log('<plugin (poll) />');
+  } else {
+    window.addEventListener('gamepadconnected', onConnected);
+    window.addEventListener('gamepaddisconnected', onDisconnected);
+  }
   Plugin.log('<plugin (start) />');
 }
 
@@ -45,6 +57,9 @@ function onConnected(event: GamepadEvent): void {
   Plugin.log('<plugin (listen) />');
 }
 
+/**
+ * Listen for gamepad inputs when connected.
+ */
 function listenGamepad(): void {
   const gamepads: Gamepad[] = navigator.getGamepads() as Gamepad[];
 
@@ -52,13 +67,36 @@ function listenGamepad(): void {
     Plugin.log('<plugin (release) />');
     return;
   }
-  const gamepad: Gamepad = gamepads[0];
+  const gamepad: Gamepad | null = gamepads[0];
 
+  if (!gamepad) {
+    return;
+  }
   plugin.gamepad = gamepad;
   const input: GamepadInput = getInput(gamepad);
 
   plugin.pushInput(input);
   requestAnimationFrame(listenGamepad);
+}
+
+/**
+ * Listen for gamepad array to manually trigger a connection/disconnection, Chrome only.
+ */
+function pollGamepad(): void {
+  const gamepads: Gamepad[] = navigator.getGamepads() as Gamepad[];
+
+  if (gamepads.length === 0) {
+    requestAnimationFrame(pollGamepad);
+    return;
+  }
+  const gamepad: Gamepad | null = gamepads[0];
+
+  if (!gamepad) {
+    onDisconnected(<any>{gamepad: gamepad});
+  } else if (plugin.gamepad === undefined) {
+    onConnected(<any>{gamepad: gamepad});
+  }
+  requestAnimationFrame(pollGamepad);
 }
 
 const digits: string = `&é"'(-è_çà)=`;
@@ -98,17 +136,18 @@ function onJoystickMoved(event: JoystickMovedEvent): void {
  * Release gamepad on disconnection event.
  */
 function onDisconnected(event: GamepadEvent): void {
-  const gamepad: Gamepad = event.gamepad;
+  const gamepad: Gamepad | null = event.gamepad;
 
   if (!plugin.gamepad) {
     return;
   }
-  if (plugin.gamepad.index !== gamepad.index) {
+  if (gamepad !== null && plugin.gamepad.index !== gamepad.index) {
     return;
   }
   plugin.gamepad = undefined;
   plugin.gui.setGamepad(false);
+  plugin.gui.updateLastInput('N/A');
   plugin.offReleased(onButtonReleased);
   plugin.offMoved(onJoystickMoved);
-  Plugin.log(`<gamepad (disconnected) index="${gamepad.index}" id="${gamepad.id}" />`);
+  Plugin.log(`<gamepad (disconnected) />`);
 }
